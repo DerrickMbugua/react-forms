ac6a1dda25627e547fd4856389ca62483761a7e5
Derrick Mwema <derrickmbugua50@gmail.com>
2022-05-12 14:18:27 +0300

    feat: controlled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 
 function App() {
   return (
@@ -18,6 +51,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <NameForm />
+      </body>
     </div>
   );
 }
