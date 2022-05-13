0b5a3d47994500acf789dd202aa440c049f16ff6
Derrick Mwema <derrickmbugua50@gmail.com>
2022-05-13 08:59:28 +0300

    forms

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import React from 'react';
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = { value: '' };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
   handleSubmit(event) {
@@ -33,6 +33,69 @@ class NameForm extends React.Component {
   }
 }
 
+class EssayFrom extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: 'Write a brief description' }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    alert('Textarea submitted')
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <textarea onChange={this.handleChange} value={this.state.value}></textarea>
+        <input type="submit" value='Submit' />
+
+      </form>
+    )
+  }
+}
+
+class SelectForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: '' }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    alert('Your favourite fruit is ' + this.state.value)
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <select onChange={this.handleChange} value={this.state.value}>
+            <option value='apple'>Apple</option>
+            <option value='banana'>Banana</option>
+            <option value='carrot'>Carrot</option>
+
+          </select>
+          <input type='submit' value='Submit' />
+
+        </form>
+      </div>
+
+    )
+  }
+}
 
 function App() {
   return (
@@ -53,6 +116,8 @@ function App() {
       </header>
       <body>
         <NameForm />
+        <EssayFrom />
+        <SelectForm />
       </body>
     </div>
   );
